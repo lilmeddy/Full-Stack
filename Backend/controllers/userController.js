f70c43cb10bb188c29c41dd4d1189329c03a9df4
lilmeddy <aghedopriscilla@gmail.com>
2023-09-23 02:41:16 +0100

    done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { userModel } from "../models/userModel.js"
 import bcryptjs from "bcryptjs"
 import { generateUserToken, verifyUserToken } from "../utilities/userToken.js";
@@ -154,53 +154,6 @@ const uploadImage = async (req,res,next) =>{
 
 export { signup, login , viewUsers, forgotPassword, resetPassword, uploadImage};
 
-import { userModel } from "../models/userModel.js";
 
-const signup = async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const checkUser = await userModel.findOne({ email: email });
-    if (checkUser) {
-      return res.status(401).send({ message: "Email used", status: false });
-    }
-    const signUp = await userModel.create({
-      firstName,
-      lastName,
-      email,
-      password,
-    });
-    console.log(signUp);
-    res.status(210).send({ message: "User Created", status: true });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Internal sever error", status: false });
-  }
-};
 
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const checkUser = await userModel.findOne({ email });
-    if (!checkUser) {
-      return res
-        .status(401)
-        .send({ message: "User not found. Please register", status: false });
-    }
-    const validate = checkUser.password == password ? true : false;
-    if (!validate) {
-      res.status(401).send({ message: "Incorrect Credentials", status: false });
-    }
-    res
-      .status(200)
-      .send({
-        message: `Login Successfully, User ${checkUser.firstName}`,
-        status: true,
-      });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Internal sever error", status: false });
-  }
-};
 
-export { signup, login };
->>>>>>> 7aca13550dd2df448ad7eb43381303832d91e589
--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import { connectDB } from "./database/connectDB.js";
 import { userRoutes } from "./routes/userRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import * as socketIo from "socket.io";
+
 
 env.config();
 const app = express();
@@ -14,7 +16,30 @@ app.use(express.urlencoded({ extended: true, limit:"100mb"}));
 app.use("/users", userRoutes)
 app.use(errorHandler)
 connectDB()
-app.listen(12000, ()=>{
+
+
+const server = app.listen(12000, ()=>{
     
     console.log("app is listening at port 12000");
 })
+
+let io = new socketIo.Server(server, {
+  cors:{
+      origin:"*"
+  }
+});
+
+
+io.on("connection", (socket)=>{
+  console.log("New user connected");
+  console.log(`User ${socket.id} is connected`);
+
+  socket.on("message", (data)=>{
+      console.log(data, socket.id);
+      io.emit("broadcast", data);
+  })
+
+  socket.on("disconnect", ()=>{
+      console.log(`User ${socket.id} has disconnected`)
+  })
+}) 
